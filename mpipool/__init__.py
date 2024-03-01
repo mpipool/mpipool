74c529a30cf465148e82a30e988f2c063e6b5285
Robin De Schepper <robin.deschepper93@gmail.com>
2024-03-01 17:51:35 +0100

    change version identifier

--- mpipool/__init__.py
+++ mpipool/__init__.py
@@ -5,7 +5,7 @@ Implementation of the futures and multiprocessing Pool interfaces based on MPI.
 __author__ = "Robin De Schepper"
 __email__ = "robingilbert.deschepper@unipv.it"
 __all__ = ["MPIExecutor", "MPIPool", "WorkerExitSuiteSignal", "enable_serde_logging"]
-__version__ = "2.2.0"
+__version__ = "2.2.1"
 
 from ._futures import MPIExecutor, WorkerExitSuiteSignal, enable_serde_logging
 from ._multiprocessing import MPIPool
